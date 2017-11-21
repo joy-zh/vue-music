4221562a431de5d613ba3fa226b2e5001d24c23a
zhangyi <zhangyi@tvmining.com>
2017-11-21 18:33:34 +0800

    a

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,135 @@
+<template>
+	<scroll class="listview" :data='data' ref="listview">
+		<ul>
+			<li v-for="group in data" class="list-group" ref="listGroup">
+				<h2 class="list-group-title">{{group.title}}</h2>
+				<ul>
+					<li v-for="item in group.items" class="list-group-item">
+						<img class="avatar" v-lazy="item.avatar" />
+						<span class="name">{{item.name}}</span>
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<div class="list-shortcut" @touchstart="onShortcutTouchStart($event)" @touchmove.stop.prevent="onShortcutTouchMove($event)">
+			<ul>
+				<li v-for="(item,index) in shortcutlist" class="item" :data-index="index" >
+					{{item}}
+				</li>
+			</ul>
+		</div>
+	</scroll>
+</template>
+
+<script>
+	const ANTHOR_HEIGHT = 18;
+	
+	import Scroll from 'base/scroll/scroll'
+	import {getData} from 'common/js/dom'
+	export default {
+		props: {
+			data: {
+				type: Array,
+				default:[]
+			}
+		},
+		computed: {
+			shortcutlist(){
+				return this.data.map((group) => {
+					return group.title.substring(0, 1);
+				})
+			}
+		},
+		created(){
+			this.touch = {};
+		},
+		methods: {
+			onShortcutTouchStart(e){
+				let authorIndex = getData(e.target,'index');
+				let firstTouch = e.touches[0];
+				this.touch.anthorIndex = authorIndex;
+				this.touch.y1 = firstTouch.pageY;
+				this.$refs.listview.scrollToElement(this.$refs.listGroup[authorIndex],0)
+			},
+			onShortcutTouchMove(e){
+				let firstTouch = e.touches[0];
+				this.touch.y2 = firstTouch.pageY;
+				let delta = (this.touch.y2 - this.touch.y1)/ANTHOR_HEIGHT | 0;
+				let authorIndex = parseInt(this.touch.anthorIndex) + delta;
+				
+				this.$refs.listview.scrollToElement(this.$refs.listGroup[authorIndex],0)
+			}
+		},
+		components: {
+			Scroll
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .listview
+    position: relative
+    width: 100%
+    height: 100%
+    overflow: hidden
+    background: $color-background
+    .list-group
+      padding-bottom: 30px
+      .list-group-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .list-group-item
+        display: flex
+        align-items: center
+        padding: 20px 0 0 30px
+        .avatar
+          width: 50px
+          height: 50px
+          border-radius: 50%
+        .name
+          margin-left: 20px
+          color: $color-text-l
+          font-size: $font-size-medium
+    .list-shortcut
+      position: absolute
+      z-index: 30
+      right: 0
+      top: 50%
+      transform: translateY(-50%)
+      width: 20px
+      padding: 20px 0
+      border-radius: 10px
+      text-align: center
+      background: $color-background-d
+      font-family: Helvetica
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color: $color-theme
+    .list-fixed
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
+</style>
