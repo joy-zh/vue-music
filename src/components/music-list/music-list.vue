fee98f802a09c72a05b4e0fe209c7857333c08d2
zhangyi <zhangyi@tvmining.com>
2018-01-17 18:34:47 +0800

    rank

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -16,7 +16,7 @@
 		<div class="bg-layer" ref="layer"></div>
 		<scroll @scroll='scroll' :data="songs" :probe-type="probeType" :listen-scroll="listenScroll" class="list" ref="list">
 			<div class="song-list-wrapper">
-				<song-list :songs="songs" @select='selectItem'></song-list>
+				<song-list :songs="songs" :rank="rank" @select='selectItem'></song-list>
 			</div>
 			<div class="loading-container" v-show="!songs.length"></div>
 		</scroll>
@@ -52,6 +52,10 @@
 			title: {
 				type: String,
 				default: ''
+			},
+			rank: {
+				type: Boolean,
+				default: false
 			}
 		},
 		data(){
